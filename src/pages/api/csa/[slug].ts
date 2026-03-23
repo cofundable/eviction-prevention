0df607ff024499efe01c1c2f21e22d83e6506983
widal001 <williamdaly422@gmail.com>
2026-03-23 01:14:13 -0400

    refactor: Changes default to static render

--- src/pages/api/csa/[slug].ts
+++ src/pages/api/csa/[slug].ts
@@ -1,3 +1,4 @@
+export const prerender = false;
 import type { APIRoute } from "astro";
 import { getCsaDetail, getCsaFeatures } from "../../../lib/db";
 import { toSlug } from "../../../lib/utils";
